af72361443591964873bda1f8daa98932d946711
Tim <timrlw@gmail.com>
2017-09-06 17:16:41 +0800

    gem fail

--- packages/metasploit-framework/build.sh
+++ packages/metasploit-framework/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://metasploit.com/
 TERMUX_PKG_DESCRIPTION="Open source penetration testing framework"
-TERMUX_PKG_VERSION=4.16.4
+TERMUX_PKG_VERSION=4.16.5
 TERMUX_PKG_DEPENDS="autoconf, bison, clang, coreutils, curl, findutils, git, apr, apr-util, libffi-dev, libgmp-dev, libpcap-dev, postgresql-dev, readline-dev, libsqlite-dev, openssl-dev, libtool, libxml2-dev, libxslt-dev, ncurses-dev, pkg-config, postgresql-contrib, wget, make, ruby-dev, libgrpc-dev, termux-tools, ncurses-utils, ncurses, unzip, zip, tar"
 
 termux_step_make_install () {
@@ -33,7 +33,5 @@ termux_step_post_massage () {
 	git checkout -- data/
 
 	gem install bundler -i $TERMUX_PREFIX/lib/ruby/gems/2.3.0
-	gem install nokogiri -- --use-system-libraries -i $TERMUX_PREFIX/lib/ruby/gems/2.3.0
-
-	bundle
+	gem install nokogiri -i $TERMUX_PREFIX/lib/ruby/gems/2.3.0 -- --use-system-libraries 
 }
